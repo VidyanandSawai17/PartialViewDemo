847d913d33fc5756ea9bc66bf6e1a805cf19e9d4
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2023-05-31 08:46:59 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewDemo.Data;
+
+namespace PartialViewDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public ProductController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            var model = applicationDbContext.Products.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartialViewDemo.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Company { get; set; }
+        public int Price { get; set; }
+    }
+
+}
